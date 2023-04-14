4b1857e3a0e689b01848c4449896bd000329ff51
aman <amanmagrawal2002@gmail.com>
2023-04-14 12:19:34 +0530

    add get request

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,17 @@
-FROM alpine:latest
+# FROM alpine:latest
 
-CMD [ "echo", "Hello world" ]
+FROM node:18-alpine
+
+RUN mkdir -p /app
+
+WORKDIR /app
+
+COPY package*.json ./
+
+COPY ./api ./api
+
+RUN npm install 
+
+EXPOSE 8080
+
+CMD [ "node", "api/index.js" ]
---
+++ api/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const fs = require('fs')
+const data1 = []
+const port = 8080
+app.use(express.json())
+app.use(cors('http://localhost:8080'))
+app.get('/greeting',(req,res)=>{
+    res.status(200).send("Hello World");
+})
+
+app.post('/employee',(req,res)=>{
+    const obj = {}
+    const name = req.body.name;
+    const city = req.body.city;
+    // if(name===''){
+    //     res.status(501).send("Mention the name")
+
+    // }
+    // obj['name'] = 
+    // fs.readFile('./data.json',"utf-8",(err,data)=>{
+    //     if(err) throw err;
+    //     if(data!=)
+    //     data1 = data;
+
+    // })
+
+    const object = fs.readFileSync('./data.json')
+    data1 = json.parse(object);
+    res.json(json.stringify(data1))
+
+})
+
+app.listen(port, ()=>{
+    console.log("Listening to:"+port);
+})
