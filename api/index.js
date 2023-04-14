74433bb14558a19d343c8ec13b19bcd6cd3c4bf8
aman <amanmagrawal2002@gmail.com>
2023-04-14 14:12:16 +0530

    add get request

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ let data1 = []
 const port = 8080
 app.use(express.json())
 app.use(cors('http://localhost:8080'))
-
+app.set('Content-type','application/json')
 
 app.get('/',(req,res)=>{
     const object = fs.readFileSync('./data.json')
@@ -29,10 +29,21 @@ app.get('/employee/all',(req,res)=>{
 res.send(data1);
 })
 
-// app.get('/employee/:id',(req,res)=>{
-
-//     res.status(200).send()
-// })
+app.get('/employee/:id',(req,res)=>{
+    const employeeId = req.params.id;
+    let obj = {}
+    const object = fs.readFileSync('./data.json');
+    // object.forEach()
+    data1 = JSON.parse(object)
+    data1.forEach(element => {
+if(element['id']==employeeId){
+obj = element;
+res.status(200).send(JSON.stringify(obj))
+}
+    });
+    res.status(404).send( { message : "Employee with "+employeeId+" was not found"});
+    
+})
 
 app.post('/employee',(req,res)=>{
     const obj = {}
@@ -54,7 +65,7 @@ app.post('/employee',(req,res)=>{
     obj['city'] = city;
     data1.push(obj);
     fs.writeFileSync('./data.json',JSON.stringify(data1));
-    res.status(201).send({"employeeId":obj["id"]})
+    res.status(201).send({"employeeId":data1.at(-1)['id']})
 }
 })
 
